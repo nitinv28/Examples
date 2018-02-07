bf7a0cfde90936bc2cafe61443698eb6935b587d
nitinv28 <nitin.v28@gmail.com>
2018-02-07 17:02:59 +0530

    comment
    comment

--- eta-zuul/src/main/java/eta/ZuluService/StartZuul.java
+++ eta-zuul/src/main/java/eta/ZuluService/StartZuul.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+//Unifide URLs
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
